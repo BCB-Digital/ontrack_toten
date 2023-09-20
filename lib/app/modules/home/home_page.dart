529d9af32d8e59132eb63a7ad3273d4475cda9e7
Yago Castelo Branco <yago.vcb@hotmail.com>
2023-09-20 18:17:52 -0300

    Construção da tela inicial do app totem

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:one_clock/one_clock.dart';
 import 'package:ontrack_toten/app/core/theme/app_colors_theme.dart';
 
@@ -21,191 +21,224 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       backgroundColor: AppColorsTheme.backgroundColor,
       body: orientacao == Orientation.portrait
-          ? Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.start,
+          ? Stack(
               children: [
-                const Image(
-                  image: AssetImage('images/ontrack_sf.png'),
-                  alignment: Alignment.center,
-                  width: double.infinity,
-                  height: 280,
-                  fit: BoxFit.cover,
-                ),
-                Container(
-                  padding: const EdgeInsets.all(12),
-                  decoration: BoxDecoration(
-                    borderRadius: const BorderRadius.all(Radius.circular(16.0)),
-                    border: Border.all(
-                      color: AppColorsTheme.secundaryColor,
-                      width: 8,
-                    ),
-                  ),
-                  child: DigitalClock(
-                    showSeconds: true,
-                    isLive: true,
-                    digitalClockTextColor: AppColorsTheme.primaryColor,
-                    textScaleFactor: 6,
-                    decoration: const BoxDecoration(
-                      shape: BoxShape.rectangle,
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(16),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 280,
+                      width: double.infinity,
+                      child: SvgPicture.asset(
+                        'images/logo_principal.svg',
+                        fit: BoxFit.cover,
                       ),
                     ),
-                    datetime: DateTime.now(),
-                  ),
-                ),
-                const Spacer(flex: 3,),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 20),
-                          decoration: BoxDecoration(
-                            borderRadius:
-                                const BorderRadius.all(Radius.circular(16.0)),
-                            color: AppColorsTheme.secundaryDarkerColor
-                                .withAlpha(90),
-                            border: Border.all(
-                              color: AppColorsTheme.secundaryColor,
-                              width: 2,
-                            ),
-                          ),
-                          child: const Text(
-                            "Matrícula",
-                            style: TextStyle(
-                                fontSize: 24,
-                                fontWeight: FontWeight.bold,
-                                color: AppColorsTheme.primaryColor,),
+                    Container(
+                      padding: const EdgeInsets.all(12),
+                      decoration: BoxDecoration(
+                        borderRadius: const BorderRadius.all(Radius.circular(16.0)),
+                        border: Border.all(
+                          color: AppColorsTheme.secundaryColor,
+                          width: 8,
+                        ),
+                      ),
+                      child: DigitalClock(
+                        showSeconds: true,
+                        isLive: true,
+                        digitalClockTextColor: AppColorsTheme.primaryColor,
+                        textScaleFactor: 6,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.rectangle,
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(16),
                           ),
                         ),
-                        const SizedBox(height: 8),
-                        Container(
-                          padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 20),
-                          decoration: BoxDecoration(
-                            borderRadius: const BorderRadius.all(
-                              Radius.circular(16.0),
-                            ),
-                            color: AppColorsTheme.secundaryDarkerColor
-                                .withAlpha(90),
-                            border: Border.all(
-                              color: AppColorsTheme.secundaryColor,
-                              width: 2,
-                            ),
-                          ),
-                          child: const Text(
-                            "Código Empresa",
-                            style: TextStyle(
-                                fontSize: 24,
-                                fontWeight: FontWeight.bold,
-                                color: AppColorsTheme.primaryColor,),
-                          ),
-                        ),
-                      ],
+                        datetime: DateTime.now(),
+                      ),
                     ),
-                    const SizedBox(width: 24),
-                    Column(
-                      children: const [
-                        SizedBox(
-                          width: 360,
-                          child: TextField(
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                              color: AppColorsTheme.primaryColor,
+                    const Spacer(
+                      flex: 2,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 20),
+                              decoration: BoxDecoration(
+                                borderRadius: const BorderRadius.all(Radius.circular(16.0)),
+                                color: AppColorsTheme.secundaryDarkerColor.withAlpha(90),
+                                border: Border.all(
+                                  color: AppColorsTheme.secundaryColor,
+                                  width: 2,
+                                ),
+                              ),
+                              child: const Text(
+                                "Matrícula",
+                                style: TextStyle(
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.bold,
+                                  color: AppColorsTheme.primaryColor,
+                                ),
+                              ),
                             ),
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(),
-                              hintText: 'Informe sua matrícula',
+                            const SizedBox(height: 8),
+                            Container(
+                              padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 20),
+                              decoration: BoxDecoration(
+                                borderRadius: const BorderRadius.all(
+                                  Radius.circular(16.0),
+                                ),
+                                color: AppColorsTheme.secundaryDarkerColor.withAlpha(90),
+                                border: Border.all(
+                                  color: AppColorsTheme.secundaryColor,
+                                  width: 2,
+                                ),
+                              ),
+                              child: const Text(
+                                "Código Empresa",
+                                style: TextStyle(
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.bold,
+                                  color: AppColorsTheme.primaryColor,
+                                ),
+                              ),
                             ),
-                          ),
+                          ],
                         ),
-                        SizedBox(height: 12),
-                        SizedBox(
-                          width: 360,
-                          child: TextField(
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                              color: AppColorsTheme.primaryColor,
+                        const SizedBox(width: 24),
+                        Column(
+                          children: const [
+                            SizedBox(
+                              width: 360,
+                              child: TextField(
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                  color: AppColorsTheme.primaryColor,
+                                ),
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  hintText: 'Informe sua matrícula',
+                                ),
+                              ),
                             ),
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(),
-                              hintText: 'Informe o código da empresa',
-                            ),
-                          ),
+                            SizedBox(height: 12),
+                            SizedBox(
+                              width: 360,
+                              child: TextField(
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                  color: AppColorsTheme.primaryColor,
+                                ),
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  hintText: 'Informe o código da empresa',
+                                ),
+                              ),
+                            )
+                          ],
                         )
                       ],
-                    )
-                  ],
-                ),
-                const Spacer(flex: 1,),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    GestureDetector(
-                      onTap: onPressedAction,
-                      child: Container(
-                        width: 320,
-                        height: 60,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(16),
-                          color:
-                              AppColorsTheme.secundaryDarkerColor.withAlpha(90),
-                          border: Border.all(
-                            color: AppColorsTheme.secundaryColor,
-                            width: 2,
+                    ),
+                    const Spacer(
+                      flex: 1,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        GestureDetector(
+                          onTap: onPressedAction,
+                          child: Container(
+                            width: 320,
+                            height: 60,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(16),
+                              color: AppColorsTheme.secundaryDarkerColor.withAlpha(90),
+                              border: Border.all(
+                                color: AppColorsTheme.secundaryColor,
+                                width: 2,
+                              ),
+                            ),
+                            child: const Center(
+                              child: Text(
+                                "Gerar Código",
+                                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold, color: AppColorsTheme.primaryColor),
+                              ),
+                            ),
                           ),
                         ),
-                        child: const Center(
-                          child: Text(
-                            "Gerar Código",
-                            style: TextStyle(
-                                fontSize: 28,
-                                fontWeight: FontWeight.bold,
-                                color: AppColorsTheme.primaryColor),
-                          ),
+                      ],
+                    ),
+                    const Spacer(
+                      flex: 6,
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(bottom: 12),
+                      child: Text(
+                        "Powered by BYG Nexus",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: AppColorsTheme.primaryColor,
                         ),
                       ),
                     ),
                   ],
                 ),
-                const Spacer(flex: 6,),
-                const Padding(
-                  padding: EdgeInsets.only(bottom: 12),
-                  child: Text(
-                    "Powered by BYG Nexus",
-                    style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: AppColorsTheme.primaryColor),
+                Positioned(
+                  left: -100,
+                  bottom: -150,
+                  height: 500,
+                  width: 450,
+                  child: SvgPicture.asset(
+                    'images/relogio_3d.svg',
+                    fit: BoxFit.cover,
                   ),
                 ),
               ],
             )
-          : Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
+          : Stack(
               children: [
-                const Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text("Tela na horizontal"),
-                ),
-                DigitalClock(
-                    showSeconds: true,
-                    isLive: true,
-                    digitalClockTextColor: Colors.black,
-                    textScaleFactor: 5,
-                    decoration: const BoxDecoration(
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text("Tela na horizontal"),
+                    ),
+                    DigitalClock(
+                      showSeconds: true,
+                      isLive: true,
+                      digitalClockTextColor: Colors.black,
+                      textScaleFactor: 5,
+                      decoration: const BoxDecoration(
                         color: Colors.yellow,
                         shape: BoxShape.rectangle,
-                        borderRadius: BorderRadius.all(Radius.circular(15))),
-                    datetime: DateTime.now()),
+                        borderRadius: BorderRadius.all(Radius.circular(15)),
+                      ),
+                      datetime: DateTime.now(),
+                    ),
+                  ],
+                ),
+                Positioned(
+                  left: -100,
+                  bottom: -150,
+                  height: 500,
+                  width: 450,
+                  child: SvgPicture.asset(
+                    'images/relogio_3d.svg',
+                    fit: BoxFit.cover,
+                  ),
+                ),
               ],
             ),
     );
